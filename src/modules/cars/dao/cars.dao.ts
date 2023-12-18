c78b43fc20bd1a834ad5caf3e23c58d52f4a04e4
asarvarjon <amriyevsarvarjon@yandex.com>
2023-12-18 18:53:28 +0500

    fix

--- src/modules/cars/dao/cars.dao.ts
+++ src/modules/cars/dao/cars.dao.ts
@@ -126,76 +126,69 @@ export default class CarsDao {
     }
  
 
-    async groupedByCategory() {
-        const groupedData = await KnexService('categories')
-          .select([
-            'categories.id as category_id',
-            'categories.name_en as category_name_en',
-            'categories.name_ru as category_name_ru',
-            // Add other category columns as needed
-          ])
-          .leftJoin('cars', 'categories.id', 'cars.category_id')
-          .groupBy('categories.id')
-          .orderBy('categories.id');
-      
-        const result = await Promise.all(groupedData.map(async (category) => {
-          const { category_id, category_name_en, category_name_ru, ...restCategoryData } = category;
-      
-          const cars = await KnexService('cars')
+    async  groupedByCategory() { 
+          const groupedData = await KnexService('categories')
             .select([
-              "categories.name_en as category.name_en",
-              "categories.id as category.id",
-              "categories.name_ru as category.name_ru",
-              "cars.*",
-              KnexService.raw(`jsonb_agg(distinct car_images) as car_images`),
-              "cities.name as city.name",
-              "cities.id as city.id",
-              "cities.slug as city.slug",
-              "cities.image_src as city.image_src",
-              "locations.name as location.name",
-              "locations.id as location.id",
-              "locations.slug as location.slug",
-              "locations.image_src as location.image_src",
-              "brands.id as brand.id",
-              "brands.title as brand.title",
-              "brands.image_src as brand.image_src",
-              "models.id as model.id",
-              "models.name as model.name",
-              "models.slug as model.slug",
+              'categories.id as category_id',
+              'categories.name_en as category_name_en',
+              'categories.name_ru as category_name_ru',
+              // Add other category columns as needed
             ])
-            .leftJoin('cities', 'cars.city_id', 'cities.id')
-            .leftJoin('locations', 'cars.location_id', 'locations.id')
-            .leftJoin('categories', 'cars.category_id', 'categories.id')
-            .leftJoin('brands', 'cars.brand_id', 'brands.id')
-            .leftJoin('models', 'cars.model_id', 'models.id')
-            .leftJoin(function () {
-              this.select([
-                'car_images.car_id',
-                "image.src as image.src"
-              ])
-                .from('car_images')
-                .leftJoin({ image: "images" }, { 'car_images.image_id': 'image.id' })
-                .groupBy('car_images.id', "image.id")
-                .as('car_images')
-            }, { 'cars.id': 'car_images.car_id' })
-            .groupBy('cars.id', 'cities.id', 'locations.id', 'models.id', 'brands.id', 'categories.id')
-            .where('cars.category_id', category_id);
-
-            let categ = {
-                id: category_id,
-                name_en: category_name_en,
-                name_ru: category_name_ru,
-                ...restCategoryData,
-              }
+            .leftJoin('cars', 'categories.id', 'cars.category_id')
+            .groupBy('categories.id')
+            .orderBy('categories.id');
       
-          return {
-            ...categ, 
-            cars,
-          };
-        }));
+          const result = await Promise.all(groupedData.map(async (category) => {
+            const { category_id, category_name_en, category_name_ru, ...restCategoryData } = category;
       
-        return result;
+            const cars = await KnexService('cars')
+  .select([
+    "cars.*",
+    "brands.id as brand.id",
+    "brands.title as brand.title",
+    "brands.image_src as brand.image_src",
+    "models.id as model.id",
+    "models.name as model.name",
+    "models.slug as model.slug",
+    KnexService.raw(`jsonb_agg(distinct car_images) as car_images`),
+  ])
+  .leftJoin('cities', 'cars.city_id', 'cities.id')
+  .leftJoin('locations', 'cars.location_id', 'locations.id')
+  .leftJoin('brands', 'cars.brand_id', 'brands.id')
+  .leftJoin('models', 'cars.model_id', 'models.id')
+  .leftJoin(function () {
+    this.select([
+      'car_images.car_id',
+      "image.src as image.src"
+    ])
+      .from('car_images')
+      .leftJoin({ image: "images" }, { 'car_images.image_id': 'image.id' })
+      .groupBy('car_images.id', "image.id")
+      .as('car_images')
+  }, { 'cars.id': 'car_images.car_id' })
+  .leftJoin('categories', 'cars.category_id', 'categories.id')  // Add this line
+  .groupBy('cars.id', 'cities.id', 'locations.id', 'models.id', 'brands.id', 'categories.id')  // Update this line
+  .where('cars.category_id', category_id);
+      
+            const categoryData = {
+              id: category_id,
+              name_en: category_name_en,
+              name_ru: category_name_ru,
+              ...restCategoryData,
+            };
+      
+            return {
+              ...categoryData,
+              cars,
+            };
+          }));
+ 
+      
+          return  result
+        
+        
       }
+      
       
       
       
