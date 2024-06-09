06013e1beb91ac0bf19849fc877e6d97adeb660f
asarvarjon <amriyevsarvarjon@yandex.com>
2024-06-09 09:44:18 +0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const ExpressApp = new App(router)
 const server = http.createServer(ExpressApp.getServer);
 
 // _________LISTEN PORT___________
-const port = 2020  
+const port = 4848  
 
 
 server.listen(port, () => console.log("Listening port on " + port)) 
